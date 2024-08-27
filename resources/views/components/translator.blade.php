9f4c9fa7181e6a0ce9daa59186dd1279cb22af69
Botmedude <dmagdalenomedina@gmail.com>
2024-08-27 11:49:36 -0600

    Vista del Traductor (No funcional)

---
+++ resources/views/components/translator.blade.php
@@ -0,0 +1,39 @@
+<div class="translator-container">
+    <img src="{{ asset('img/Welcome.jpeg') }}" alt="Traductor de lenguas" class="translator-img">
+    <h1 class="translator-title">Traductor</h1>
+    <p class="translator-subtitle">De lenguas</p>
+
+    <div class="translator-options">
+        <div>
+            <p>Texto</p>
+            <select class="translator-select">
+                <option value="es">Elegir idioma</option>
+                <option value="nah">Náhuatl</option>
+                <option value="maya">Maya</option>
+                <option value="mixteco">Mixteco</option>
+                <option value="zapoteco">Zapoteco</option>
+                <option value="tzotzil">Tzotzil</option>
+            </select>
+        </div>
+        <div>
+            <p>Imagen/Foto</p>
+            <button class="translator-button">Subir</button>
+        </div>
+        <div>
+            <p>Voz</p>
+            <select class="translator-select">
+                <option value="es">Elegir idioma</option>
+                <option value="nah">Náhuatl</option>
+                <option value="maya">Maya</option>
+                <option value="mixteco">Mixteco</option>
+                <option value="zapoteco">Zapoteco</option>
+                <option value="tzotzil">Tzotzil</option>
+            </select>
+        </div>
+    </div>
+
+    <div class="translator-textarea">
+        <textarea class="translator-text" placeholder="Texto"></textarea>
+        <textarea class="translator-text" placeholder="Resultado"></textarea>
+    </div>
+</div>
